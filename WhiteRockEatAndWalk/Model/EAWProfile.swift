7da6451b566330a4223ab75cf6a2c31e27e6b1d5
AndreFoulem <timmycasual@icloud.com>
2023-03-22 18:47:18 -0700

    Create EAWProfile model

---
+++ WhiteRockEatAndWalk/Model/EAWProfile.swift
@@ -0,0 +1,40 @@
+//
+//  EAWProfile.swift
+//  WhiteRockEatAndWalk
+//
+//  Created by AndreMacBook on 2023-03-22.
+//
+
+import Foundation
+
+import CloudKit
+
+struct EAWProfile {
+  
+  static let kFirstName = "firstName"
+  static let kLastName = "lastName"
+  static let kAvatar = "avatar"
+  static let kCompany = "company"
+  static let kBio = "bio"
+  static let kHasReviewed = "hasReviewed"
+
+  
+  let ckRecordID: CKRecord.ID
+  let firstName: String
+  let lastName: String
+  let avatar: CKAsset!
+  let company: String
+  let bio: String
+  let hasReviewed: CKRecord.Reference? = nil
+}
+
+extension EAWProfile {
+  init(record: CKRecord) {
+    ckRecordID = record.recordID
+    firstName = record[Self.kFirstName] as? String ?? "N/A"
+    lastName = record[Self.kLastName] as? String ?? "N/A"
+    avatar = record[Self.kAvatar] as? CKAsset
+    company = record[Self.kCompany] as? String ?? "N/A"
+    bio = record[Self.kBio] as? String ?? "N/A"
+  }
+}
