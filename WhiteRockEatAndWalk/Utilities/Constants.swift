26ac3df88c82072ce9dff2081ae74ba00a668542
AndreFoulem <timmycasual@icloud.com>
2023-03-25 14:47:17 -0700

    Add PlaceHolder (UIImage) as enum

--- WhiteRockEatAndWalk/Utilities/Constants.swift
+++ WhiteRockEatAndWalk/Utilities/Constants.swift
@@ -5,9 +5,17 @@
 //  Created by AndreMacBook on 2023-03-22.
 //
 
-import Foundation
+import UIKit
 
 enum RecordType {
   static let location = "EAWLocation"
   static let profile = "EAWProfile"
 }
+
+enum PlaceHolderImage {
+  static let avatarImg = UIImage(named: "default-avatar")
+  static let squareImg = UIImage(named: "default-square-asset")
+  static let bannerImg = UIImage(named: "default-banner-asset")
+}
+
+
