5a0097d815d7ea525e04ccbeeb745943b455ecfb
AndreFoulem <timmycasual@icloud.com>
2023-03-27 14:58:21 -0700

    Create Onboard content layout

---
+++ WhiteRockEatAndWalk/Content/OnboardContentEnum.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardContentEnum.swift
+//  WhiteRockEatAndWalk
+//
+//  Created by AndreMacBook on 2023-03-27.
+//
+
+import Foundation
+
+enum OnboardContent: String {
+  case SFSymbol1 = "building.2.crop.circle"
+  case SFSymbol2 = "checkmark.circle"
+  case SFSymbol3 = "person.2.circle"
+  case title1 = "Restaurants Locations"
+  case title2 = "Give a review"
+  case title3 = "Find Reviews"
+  case desc1 = "Find Ice Cream and Fish&Chips places around White Rock BoardWalk"
+  case desc2 = "Give a review of your favorite White Rock spot"
+  case desc3 = "Find the best ice creams and fish&chips!"
+}
+
+
--- WhiteRockEatAndWalk/Screens (Views)/OnBoardingView.swift
+++ WhiteRockEatAndWalk/Screens (Views)/OnBoardingView.swift
@@ -8,24 +8,59 @@
 import SwiftUI
 
 struct OnBoardingView: View {
+
     var body: some View {
       VStack {
         LogoView(frameWidth: 250)
-        HStack {
-          Image(systemName: "building.2.crop.circle")
-            .resizable()
-            .frame(width: 50, height: 50)
-            .foregroundColor(.accentColor)
-          VStack {
-            Text("Restaurants Locations").bold()
-            Text("Find Ice Cream and Fish & Chips places at White Rock boardwalk.")
-              .foregroundColor(.secondary)
-              .lineLimit(2)
-              .minimumScaleFactor(0.7)
-          }
+          .padding(.bottom, 24)
+      
+        VStack(alignment: .leading) {
+          InfoCell( imageName: OnboardContent.SFSymbol1.rawValue,
+                    title: OnboardContent.title1.rawValue,
+                    description: OnboardContent.desc1.rawValue)
+          
+          InfoCell( imageName: OnboardContent.SFSymbol2.rawValue,
+                    title: OnboardContent.title2.rawValue,
+                    description: OnboardContent.desc2.rawValue)
+          InfoCell( imageName: OnboardContent.SFSymbol3.rawValue,
+                    title: OnboardContent.title3.rawValue,
+                    description: OnboardContent.desc3.rawValue)
+        }//vs
+        .padding(.trailing, 32)
+        .padding(.leading, 16)
+        
+      }//vs
+    }//body
+}
+
+extension OnBoardingView {
+  
+  struct InfoCell: View {
+    
+    var imageName: String
+    var title: String
+    var description: String
+    
+    var body: some View {
+      HStack(spacing: 16) {
+        Image(systemName: imageName)
+          .resizable()
+          .frame(width: 50, height: 50)
+          .foregroundColor(.accentColor)
+          .padding(.vertical, -38)
+        VStack(alignment: .leading) {
+          Text(title).bold()
+            .padding(.bottom, 1)
+          Text(description)
+            .foregroundColor(.secondary)
+            .lineLimit(2)
+            .minimumScaleFactor(0.7)
         }
       }
+      .padding(.bottom, 12)
     }
+  }//struct Description
+  
 }
 
 struct OnBoardingView_Previews: PreviewProvider {
