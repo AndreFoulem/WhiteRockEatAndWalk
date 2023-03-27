ca5684bcd497c7feeac75fcceac5780f48b9c1d3
AndreFoulem <timmycasual@icloud.com>
2023-03-27 16:04:41 -0700

    Create button label file

---
+++ WhiteRockEatAndWalk/Custom Views/buttonLabels/DismissLabelForButton.swift
@@ -0,0 +1,20 @@
+//
+//  DismissLabelForButton.swift
+//  WhiteRockEatAndWalk
+//
+//  Created by AndreMacBook on 2023-03-27.
+//
+
+import SwiftUI
+
+struct DismissLabelForButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DismissLabelForButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DismissLabelForButton()
+    }
+}
